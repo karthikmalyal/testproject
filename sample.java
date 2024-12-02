c83e221c6f921e77e74100c7fede17785ec5f4a5
karthik <karthikmalyala575@gmail.com>
2024-12-02 15:38:20 +0530

    sample java

--- sample.java
+++ sample.java
@@ -1,6 +1,12 @@
-abstract class Shape 
-{
-    int color;
-    // An abstract function
-    abstract void draw();
+public class sample {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
 }
